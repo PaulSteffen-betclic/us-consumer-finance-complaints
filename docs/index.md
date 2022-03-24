6ee5dfea5ed17f5bcaa54db0cc43784e6223aec0
p.steffen@betclicgroup.com <p.steffen@betclicgroup.com>
2022-03-24 14:39:03 +0100

    initial commit

---
+++ docs/index.md
@@ -0,0 +1,3 @@
+# Welcome to us-consumer-finance-complaints
+
+US consumer complaints on financial products and company responses
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='US consumer complaints on financial products and company responses',
+    author='Paul Steffen',
+    license='MIT',
+)
